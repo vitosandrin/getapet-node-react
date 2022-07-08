66e1e3c087b3447865d397af0fc4fb0e00d4e195
Vito <sandrinvito10@gmail.com>
2022-07-08 03:08:00 -0300

    fix: connect db

--- backend/db/conn.js
+++ backend/db/conn.js
@@ -1,10 +1,10 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose")
 
-async function main(){
-    await mongoose.connect('mongodb://localhost:27017/getapet')
-    console.log("!conectou ao mongoose!")
+async function main() {
+    await mongoose.connect("mongodb://localhost:27017/getapet")
+    console.log("Conectado ao mongoose!")
 }
 
-main().catch((err) => console.log(err))
+main().catch(err => console.log(err))
 
 module.exports = mongoose
